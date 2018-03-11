ade1c4c7a6965dac0ae67e6030369e19c6d54e89
Lakpa Sherpa <sherpalakpa18@gmail.com>
2018-03-11 21:37:04 +0545

    Sqlite database added

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-#basicRE
+# basicRE
 This recommendation system is written with server side language and uses data dictionary to it’s input for prediction. So the application user is responsible for feeding data to engine. To remove code redundancy, it is written in OOP which can used by creating it’s object when required.
+
 This simplified filtering system can be used on a site with a few thousand items and members. But as the number of items grows it will be time consuming to calculate the recommendations every time someone purchases or browses an item. It assumes that developer has feed input according to user – item format.
+<a href="http://slakpa.com.np/index.php/portfolio/recommender-system-php">Go to website!</a>
--- sqlite_dataset.php
+++ sqlite_dataset.php
@@ -6,24 +6,26 @@
 
 include_once 'Recommender.php';
 
-$con = mysql_connect('localhost','root','');
-mysql_select_db('majorproject');
+$myPDO = new PDO('sqlite:database.sqlite');
 $sql1 = "SELECT * FROM `users`";
 $sql2 = "SELECT * FROM `books`";
-$r2 = mysql_query($sql2);
+// $r2 = mysql_query($sql2);
+$r2 = $myPDO->query($sql2);
 $books = [];
-while ($row2 = mysql_fetch_assoc($r2)) {
+foreach($r2 as $row2) {
 	$books[$row2['id']] = $row2['name'];
 }
-$r1 = mysql_query($sql1);
+
+$r1 = $myPDO->query($sql1);
 $array = [];
 $subArray = [];
-while($row1 = mysql_fetch_assoc($r1)){
+foreach($r1 as $row1){
 	$id = $row1['userid'];
 	$name = $row1['name'];
 	$sql3 = "SELECT * FROM `ratings` WHERE user_id = $id";
-	$r3 = mysql_query($sql3);
-	while ($row3 = mysql_fetch_assoc($r3)) {	
+
+	$r3 = $myPDO->query($sql3);
+	foreach($r3 as $row3){
 		$subArray[$row3['book_id']] = $row3['rating'];
 	}
 	$array[$name] = $subArray;
